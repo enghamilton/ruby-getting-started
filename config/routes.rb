34ace0333061922946b81a9366d3cc589a5af5ec
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-12-25 16:49:41 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   root 'welcome#index'
+  	
+  get 'home/saudacao'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
