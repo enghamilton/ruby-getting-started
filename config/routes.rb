29070a8b077d5f4c622450e43a43c5b128906120
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-12-28 22:10:29 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   	
-  get 'home/product' 
+  get 'home#product' 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
