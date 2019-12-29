5f078efb96605ccd5ffd34a2622d8355297737e6
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2019-12-28 21:21:53 -0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
      
     def saudacao
-        @mensagem = “Hello world”
+        #@mensagem = “Hello world”
+        @message = ‘Hello World!’
         #@mensagem = {id:1, name:'user1 hamilton', phone:'912345678'}
     end
   
