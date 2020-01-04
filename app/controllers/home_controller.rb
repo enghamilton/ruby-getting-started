6f3cd7165ebced68298258e9e26e79bc9fc693d3
Hamilton Kamiya <31168921+enghamilton@users.noreply.github.com>
2020-01-04 18:42:10 -0300

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
+require 'rubygems'
+require 'mechanize'
+
 class HomeController < ApplicationController
      
     def product
-        @mensagem = "Hello world"
+        agent = Mechanize.new
+        page = agent.get("http://pizzaria2.000webhostapp.com/android_conect/get_all_products.php")
+        @message = page
+        #@message = "Hello world"
         #@mensagem = “Hello world”
         #@message = ‘Hello World!’
         #@mensagem = {id:1, name:'user1 hamilton', phone:'912345678'}
